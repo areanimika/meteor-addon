f582a961d708d74276e870eea72df6910482d40b
Areanimika <areanimika@gmail.com>
2024-03-01 11:37:08 +0200

    Kill Effects - Added sound related configuration options.

--- src/main/java/com/areanimika/areanimikahax/mixinterface/ILightningEntity.java
+++ src/main/java/com/areanimika/areanimikahax/mixinterface/ILightningEntity.java
@@ -1,9 +1,22 @@
 package com.areanimika.areanimikahax.mixinterface;
 
 import meteordevelopment.meteorclient.utils.render.color.Color;
+import net.minecraft.sound.SoundCategory;
 
 public interface ILightningEntity {
     Color ama$getColorOverride();
 
     void ama$setColor(Color color);
+
+    double ama$getThunderVolume();
+
+    void ama$setThunderVolume(double volume);
+
+    double ama$getImpactVolume();
+
+    void ama$setImpactVolume(double volume);
+
+    SoundCategory ama$getSoundCategory();
+
+    void ama$setSoundCategory(SoundCategory category);
 }
